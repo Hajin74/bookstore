90f618cfab33525d4a0907850eba54ccd1f3b911
mean-g <82minique@gmail.com>
2024-03-12 20:56:11 +0900

    Revert "Merge branch 'feature/cart' into 'master'"
    This reverts commit 1871abb717eed711a654c137e64d461b57e89b29, reversing
    changes made to 01d9ab87f02031affb912e9c3e837696c55e53ea.

--- src/main/java/eliceproject/bookstore/cart/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package eliceproject.bookstore.cart;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-    Optional<Cart> findCartByUser(User user);
-}
